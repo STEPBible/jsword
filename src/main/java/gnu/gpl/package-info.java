917b63ac7b6d2b10fc2fe0282872884a51eec4e0
Daniel Bennett <danbenn@umich.edu>
2023-01-07 09:54:03 +0300

    remove redundant gnu package

--- src/main/java/gnu/gpl/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
-This package contains GNU General Public License information only.
-*/
-package gnu.gpl;
--- src/main/java/gnu/lgpl/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
-This package contains GNU Lesser General Public License information only.
-*/
-package gnu.lgpl;
