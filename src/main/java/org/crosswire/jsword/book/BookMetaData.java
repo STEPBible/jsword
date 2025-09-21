04074dce43fa4e3adfa5b055db4c1a169b799829
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2025-09-20 23:34:42 -0700

    Read the Scope in the config file

--- src/main/java/org/crosswire/jsword/book/BookMetaData.java
+++ src/main/java/org/crosswire/jsword/book/BookMetaData.java
@@ -269,6 +269,7 @@ public interface BookMetaData extends Comparable<BookMetaData> {
      * @return the current scope of the book.
      */
     VerseKey getScope();
+    String getScopeInConfig();
 
     /**
      * Saves an entry to a particular configuration file.
